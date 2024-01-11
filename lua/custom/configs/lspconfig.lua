244850eba6a8f5ff1df1c737b3c5d4e7ca401991
sai-sy <saihaansyed2003@gmail.com>
2024-01-11 03:31:11 -0500

    readme

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,26 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "tsserver", "clangd", "gopls" }
+local util = require "lspconfig/util"
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = {"go","gomod","gowork","gompl"},
+  root_dir = util.root_pattern("go_work","go_mod",".git"),
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- 
+-- lspconfig.pyright.setup { blabla}
