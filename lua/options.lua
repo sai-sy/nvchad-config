1ae81a7eccf2e553f40c87a1e1d767c5317ccc73
sai <saihaansyed2003@gmail.com>
2025-11-22 10:02:09 -0500

    2 tab space

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,12 @@ require "nvchad.options"
 
 -- add yours here!
 
+local opt = vim.opt
+
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
