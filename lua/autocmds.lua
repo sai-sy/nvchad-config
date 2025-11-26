6e6544e5778a3afd508dcf925154d93680498ae1
sai <saihaansyed2003@gmail.com>
2025-11-26 06:53:44 -0500

    2 space tabs

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,13 @@
 require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("FileType", {
+  pattern = { "markdown", "md" },
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
