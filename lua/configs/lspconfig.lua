4f38dcebfbef1ccbff377b9dbdc35e4212d54b6d
sai <saihaansyed2003@gmail.com>
2025-11-30 21:53:30 -0500

    turns out everything was fine with lsp hover

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -12,27 +12,6 @@ local servers = {
 }
 vim.lsp.enable(servers)
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    if not args.data then
-      return
-    end
-
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-
-    if not client then
-      return
-    end
-
-    if client.server_capabilities.signatureHelpProvider then
-      local ok, signature = pcall(require, "nvchad.lsp.signature")
-      if ok then
-        signature.setup(client, args.buf)
-      end
-    end
-  end,
-})
-
 -- read :h vim.lsp.config for changing options of lsp servers 
 
 
